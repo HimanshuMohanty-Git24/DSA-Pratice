7ae90ba82c92f3a18d203c2d9aa0c1db1245cd16
Himanshu Mohanty <codehimanshu24@gmail.com>
2022-07-26 20:33:04 +0530

    Ques

---
+++ Basic Concept/Array.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    int a[5] = {2, 4, 6, 8, 10};
+    for (int i = 0; i < 5; i++)
+    {
+        printf("%d\n", a[i]);
+    }
+
+    return 0;
+}
