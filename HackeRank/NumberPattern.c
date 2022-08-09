043b29887875b7b6eebffbbe30412491802dd581
Himanshu Mohanty <codehimanshu24@gmail.com>
2022-08-09 21:34:25 +0530

    Num pattern Ques

--- HackeRank/NumberPattern.c
+++ HackeRank/NumberPattern.c
@@ -1,16 +1,47 @@
 #include <stdio.h>
 
-int main(int argc, char const *argv[])
+int main()
 {
-    int n = 4;
-    int len = ((n * 2) - 1);
-    for (int i = 0; i < len; i++)
+    int n;
+    scanf("%d", &n);
+    int size = (2 * n) - 1;
+    int mat[size][size];
+    int top = 0;
+    int right = (2 * n) - 2;
+    int bottom = (2 * n) - 2;
+    int left = 0;
+    while (n > 0)
     {
-        for (int j = 0; j < len; j++)
+        for (int i = left; i <= right; i++)
         {
-            printf("4 ");
+            mat[top][i] = n;
+        }
+        for (int i = top; i <= bottom; i++)
+        {
+            mat[i][right] = n;
+        }
+        for (int i = right; i >= left; i--)
+        {
+            mat[right][i] = n;
+        }
+        for (int i = bottom; i >= top; i--)
+        {
+            mat[i][top] = n;
+        }
+        top++;
+        right--;
+        bottom--;
+        left++;
+        n--;
+    }
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < size; j++)
+        {
+            printf("%d ", mat[i][j]);
         }
         printf("\n");
     }
+
     return 0;
 }
