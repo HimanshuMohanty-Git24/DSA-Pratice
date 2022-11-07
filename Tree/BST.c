2f1fcdb677cb2c30c377eb37316eb6dec1e999b6
Himanshu Mohanty <codehimanshu24@gmail.com>
2022-11-07 22:01:00 +0530

    BST

---
+++ Tree/BST.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct node
+{
+    int data;
+    struct node *left, *right;
+};
+struct node *create()
+{
+    int x;
+    struct node *newnode;
+    newnode = (struct node *)malloc(sizeof(struct node));
+    printf("\nEnter the data you want to insert:");
+    scanf("%d", &x);
+    if (x == -1)
+    {
+        return 0;
+    }
+    newnode->data = x;
+    printf("Enter left child of %d", x);
+    newnode->left = create();
+    printf("Enter right child of %d", x);
+    newnode->right = create();
+    return newnode;
+}
+int main()
+{
+    struct node *root;
+    root = create();
+}
