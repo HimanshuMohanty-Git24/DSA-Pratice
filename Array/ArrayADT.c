57d40c90fa3037ab3baa225b8e1b6f9ce36ced13
Himanshu Mohanty <codehimanshu24@gmail.com>
2022-12-17 22:24:00 +0530

    Ques

--- Array/ArrayADT.c
+++ Array/ArrayADT.c
@@ -206,6 +206,76 @@ void reverse(struct Array *arr)
         arr->A[i] = temp;
     }
 }
+void sortedInsert(int key, struct Array *arr)
+{
+    int i = arr->length - 1;
+    while (arr->A[i] > key)
+    {
+        arr->A[i] = arr->A[i - 1];
+        i--;
+    }
+    arr->A[i + 1] = key;
+}
+void sortedCheck(struct Array arr)
+{
+    int flag = 0;
+    for (int i = 0; i < arr.length - 1; i++)
+    {
+        if (arr.A[i] > arr.A[i + 1])
+        {
+            flag = 1;
+            break;
+        }
+    }
+    if (flag == 0)
+    {
+        printf("\nArray is sorted !!\n");
+    }
+    else if (flag == 1)
+    {
+        printf("\nArray is not sorted !!\n");
+    }
+}
+void negPosSides(struct Array *arr)
+{
+    int i = 0;
+    int j = arr->length - 1;
+    while (arr->A[i] < 0)
+    {
+        i++;
+    }
+    while (arr->A[j] >= 0)
+    {
+        j--;
+    }
+    int temp = arr->A[i];
+    arr->A[i] = arr->A[j];
+    arr->A[j] = temp;
+}
+struct Array *Merge(struct Array *arr1, struct Array *arr2)
+{
+    int i, j, k;
+    i = j = k = 0;
+
+    struct Array *arr3 = (struct Array *)malloc(sizeof(struct
+                                                       Array));
+
+    while (i < arr1->length && j < arr2->length)
+    {
+        if (arr1->A[i] < arr2->A[j])
+            arr3->A[k++] = arr1->A[i++];
+        else
+            arr3->A[k++] = arr2->A[j++];
+    }
+    for (; i < arr1->length; i++)
+        arr3->A[k++] = arr1->A[i];
+    for (; j < arr2->length; j++)
+        arr3->A[k++] = arr2->A[j];
+    arr3->length = arr1->length + arr2->length;
+    arr3->size = 10;
+
+    return arr3;
+}
 int main()
 {
     int index, ele;
