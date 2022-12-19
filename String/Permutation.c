5ad9f3541043adc143ed1c19795b3f8b7e92eb1d
Himanshu Mohanty <codehimanshu24@gmail.com>
2022-12-19 21:54:45 +0530

    Permulation of string

---
+++ String/Permutation.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+void perm(char s[], int k)
+{
+    static int A[10] = {0};
+    static char Res[10];
+    int i;
+    if (s[k] == '\0')
+    {
+        Res[k] = '\0';
+        printf("%s\n", Res);
+    }
+    else
+    {
+        for (i = 0; s[i] != '\0'; i++)
+        {
+            if (A[i] == 0)
+            {
+                Res[k] = s[i];
+                A[i] = 1;
+                perm(s, k + 1);
+                A[i] = 0;
+            }
+        }
+    }
+}
+int main()
+{
+    char s[] = "ABC";
+    perm(s, 0);
+    return 0;
+}
