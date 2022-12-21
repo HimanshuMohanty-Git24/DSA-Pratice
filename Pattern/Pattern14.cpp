3d2277e073521fc7950b7644f55426955e6cc2ee
Himanshu Mohanty <codehimanshu24@gmail.com>
2022-12-21 22:14:42 +0530

    p14

---
+++ Pattern/Pattern14.cpp
@@ -0,0 +1,27 @@
+/*A Pattern 14
+B C
+D E F
+G H I J*/
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    int i = 1;
+    char ch = 'A';
+    while (i <= n)
+    {
+        int j = 1;
+        while (j <= i)
+        {
+            cout << ch << " ";
+            ch++;
+            j++;
+        }
+        cout << endl;
+        i++;
+    }
+
+    return 0;
+}
