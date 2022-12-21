e4d4e41cb3159071823f9e615b279b7e67c44d25
Himanshu Mohanty <codehimanshu24@gmail.com>
2022-12-21 23:50:30 +0530

    p17

---
+++ Pattern/Pattern17.cpp
@@ -0,0 +1,30 @@
+/*  * pattern 17
+ **
+ ****/
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    int i = 1;
+    while (i <= n)
+    {
+        int space = n - i;
+        while (space)
+        {
+            cout << " ";
+            space--;
+        }
+        int j = 1;
+        while (j <= i)
+        {
+            cout << "*";
+            j++;
+        }
+        cout << endl;
+        i++;
+    }
+
+    return 0;
+}
