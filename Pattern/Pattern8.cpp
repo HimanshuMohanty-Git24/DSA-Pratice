a2716230950839856e7398a52aa64399224c6ae6
Himanshu Mohanty <codehimanshu24@gmail.com>
2022-12-21 21:48:50 +0530

    pattern 8

--- Pattern/Pattern8.cpp
+++ Pattern/Pattern8.cpp
@@ -1,7 +1,7 @@
-/*1 Pattern 7
-2 3
-4 5 6
-7 8 9 10*/
+/*1 Pattern 8
+2 1
+3 2 1
+4 3 2 1*/
 #include <bits/stdc++.h>
 using namespace std;
 int main()
@@ -9,12 +9,13 @@ int main()
     int n;
     cin >> n;
     int i = 1;
+    int count = 1;
     while (i <= n)
     {
         int j = 1;
         while (j <= i)
         {
-            cout << i + j - 1 << " "; // Logic
+            cout << i - j + 1 << " "; // Logic
             j++;
         }
         cout << endl;
