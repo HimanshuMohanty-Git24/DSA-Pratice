54811307af3f594a98f032cbf8313196f009ea24
Himanshu Mohanty <codehimanshu24@gmail.com>
2022-12-22 08:58:47 +0530

    P19

---
+++ Pattern/Pattern19.cpp
@@ -0,0 +1,31 @@
+/***** Pattern 19
+ ***
+ **
+ **/
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    int i = 1;
+    while (i <= n)
+    {
+        int space = i - 1;
+        while (space)
+        {
+            cout << " ";
+            space--;
+        }
+        int j = 1;
+        while (j <= n - i + 1)
+        {
+            cout << "*";
+            j++;
+        }
+        cout << endl;
+        i++;
+    }
+
+    return 0;
+}
