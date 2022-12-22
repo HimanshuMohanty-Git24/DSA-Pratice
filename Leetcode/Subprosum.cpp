4fd342471d7f2751686c95f54d357dbdd0583b4c
Himanshu Mohanty <codehimanshu24@gmail.com>
2022-12-22 15:33:50 +0530

    Ques

--- Leetcode/Subprosum.cpp
+++ Leetcode/Subprosum.cpp
@@ -6,11 +6,11 @@ int subtractProductAndSum(int n)
 {
     int sum = 0;
     int pro = 1;
-    while (n != 0)
+    while (n != 0) // till num is not reduced to zero
     {
-        sum = sum + (n % 10);
-        pro = pro * (n % 10);
-        n = n / 10;
+        sum = sum + (n % 10); // sum of each digit
+        pro = pro * (n % 10); // product of each digit
+        n = n / 10;           // reduce the num expression by removing the last part
     }
     int res = pro - sum;
     return res;
