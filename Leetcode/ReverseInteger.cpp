528abcb7b2b8b8fada7c3f09d15b6a966b3b7c49
Himanshu Mohanty <codehimanshu24@gmail.com>
2022-12-23 22:52:33 +0530

    Reverse integer

--- Leetcode/ReverseInteger.cpp
+++ Leetcode/ReverseInteger.cpp
@@ -7,21 +7,15 @@ int reverse(int x)
     while (x != 0)
     {
         r = x % 10;
-        if ((rev > INT_MAX / 10) || (rev < INT_MIN / 10))
+        if ((rev > INT_MAX / 10) || (rev < INT_MIN / 10)) // here we check that is a*10 is out of our range if it is out of int range we return 0
         {
             return 0;
         }
         x = x / 10;
-        rev = rev * 10 + r;
+        rev = rev * 10 + r; // Here it may go out of the space constrain in the question so we put a check at the top to stop this exception
     }
-    if (sizeof(rev) >= 32)
-    {
-        return 0;
-    }
-    else
-    {
-        return rev;
-    }
+
+    return rev;
 }
 int main()
 {
