ffb2370aed3af77161282f9bf2b371000c57b831
Himanshu Mohanty <codehimanshu24@gmail.com>
2022-12-24 09:29:07 +0530

    No of notes

--- Numbersys/NoOfNotes.cpp
+++ Numbersys/NoOfNotes.cpp
@@ -4,37 +4,26 @@ int main()
 {
     int n100 = 0, n50 = 0, n20 = 0, n01 = 0;
     int amount = 5555;
-    switch (amount >= 100)
+    switch (1)
     {
     case 1:
         n100 = amount / 100;
-        amount -= n100 * 100;
-        break;
-    }
-    switch (amount >= 50)
-    {
-    case 1:
+        amount = amount - (n100 * 100);
+    case 2:
         n50 = amount / 50;
-        amount -= n50 * 50;
-    }
-    switch (amount >= 20)
-    {
-    case 1:
+        amount = amount - (n50 * 50);
+    case 3:
         n20 = amount / 20;
-        amount -= n20 * 20;
+        amount = amount - (n20 * 20);
+    case 4:
+        n01 = amount / 1;
+        amount = amount - (n01 * 1);
+    default:
         break;
     }
-    switch (amount >= 1)
-    {
-    case 1:
-        n01 = amount / 10;
-        amount -= n01 * 10;
-        break;
-    }
-    cout << "100 RS notes:" << n100 << endl;
-    cout << "50 RS notes:" << n50 << endl;
-    cout << "20 RS notes:" << n20 << endl;
-    cout << "1 RS notes:" << n01 << endl;
-
+    cout << "No. of 100 RS notes-->" << n100 << endl;
+    cout << "No. of 50 RS notes-->" << n50 << endl;
+    cout << "No. of 20 RS notes-->" << n20 << endl;
+    cout << "No. of 1 RS notes-->" << n01 << endl;
     return 0;
 }
