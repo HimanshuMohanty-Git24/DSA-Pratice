07dbccf95dd1d82000a9e84cab1c7d2e2893628a
Himanshu Mohanty <codehimanshu24@gmail.com>
2023-06-02 21:48:28 +0530

    Pattern

---
+++ Warmup/1234Box.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    cout << "\nEnter the value of n\n";
+    int n;
+    cin >> n;
+    cout << "\nn=" << n << endl;
+    cout << "\nPattern 1:\n";
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << j + 1 << " ";
+        }
+        cout << endl;
+    }
+    cout << "\nPattern 2:\n";
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << n - j << " ";
+        }
+        cout << endl;
+    }
+    cout << "\nPattern 3:\n";
+    static int ctr = 1;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << ctr++ << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
