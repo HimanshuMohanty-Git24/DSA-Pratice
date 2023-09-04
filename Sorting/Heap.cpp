73ea87782b0231532efbac5d1be8925bb1527c49
HimanshuMohanty-Git24 <codehimanshu24@gmail.com>
2023-09-04 13:00:10 +0530

    Added Heapify Algo
    Co-authored-by: Himanshu Mohanty <codehimanshu24@gmail.com>

--- Sorting/Heap.cpp
+++ Sorting/Heap.cpp
@@ -66,7 +66,21 @@ public:
     }
   }
 };
-
+void Heapify(int arr[],int n,int i){
+  int largest =i;
+  int left=2*i;
+  int right = 2*i+1;
+  if(left<n&&arr[largest]<arr[left]){
+    largest=left;
+  }
+  if(right<n&&arr[largest]<arr[right]){
+    largest=right;
+  }
+  if(largest!=i){
+    swap(arr[i],arr[largest]);
+    Heapify(arr,n,largest);
+  }
+}
 int main() {
   Heap h;
   h.insert(50);
@@ -85,5 +99,15 @@ int main() {
   h.print();
   cout << endl;
 
+  int arr[6]={-1,54,53,55,52,50};
+  int n=5;
+  for (int i = n/2; i > 0; i--)
+  {
+    Heapify(arr,n,i);
+  }
+  cout<<"Printing the array"<<endl;
+  for(int i=1;i<=n;i++){
+    cout<<arr[i]<<" ";
+  }
   return 0;
 }
