5a38d0c6e0c98fd9206f1e2f4450a32638567bb5
HimanshuMohanty-Git24 <codehimanshu24@gmail.com>
2023-09-04 12:42:59 +0530

    Heap Dletion updated

--- Sorting/Heap.cpp
+++ Sorting/Heap.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 using namespace std;
+
 class Heap {
 public:
   int arr[100];
   int size = 0;
+
   Heap() {
     arr[0] = -1;
     size = 0;
@@ -25,18 +27,63 @@ public:
       }
     }
   }
+
   void print() {
     for (int i = 1; i <= size; i++) {
       cout << arr[i] << " ";
     }
   }
+
+  void Delete() {
+    if (size == 0) {
+      cout << "Heap is empty" << endl;
+      return;
+    }
+
+    arr[1] = arr[size];
+    size--;
+
+    int i = 1;
+    while (i * 2 <= size) {
+      int left = i * 2;
+      int right = i * 2 + 1;
+      int largest = i;
+
+      if (arr[left] > arr[largest]) {
+        largest = left;
+      }
+
+      if (right <= size && arr[right] > arr[largest]) {
+        largest = right;
+      }
+
+      if (largest != i) {
+        swap(arr[i], arr[largest]);
+        i = largest;
+      } else {
+        break;
+      }
+    }
+  }
 };
+
 int main() {
   Heap h;
   h.insert(50);
   h.insert(55);
+  h.insert(54);
   h.insert(53);
   h.insert(52);
+
+  cout << "Heap before deletion: ";
   h.print();
+  cout << endl;
+
+  h.Delete();
+
+  cout << "Heap after deletion: ";
+  h.print();
+  cout << endl;
+
   return 0;
 }
