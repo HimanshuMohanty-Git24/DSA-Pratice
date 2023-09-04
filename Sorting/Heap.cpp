fe0376eca57f8aef708521d28c27ac7ae0cf58a3
Himanshu Mohanty <codehimanshu24@gmail.com>
2023-09-04 10:59:32 +0530

    HeapInsertion

--- Sorting/Heap.cpp
+++ Sorting/Heap.cpp
@@ -1,8 +1,42 @@
 #include <iostream>
-#include <vector>
 using namespace std;
-int main()
-{
+class Heap {
+public:
+  int arr[100];
+  int size = 0;
+  Heap() {
+    arr[0] = -1;
+    size = 0;
+  }
 
-    return 0;
+  void insert(int val) {
+    size = size + 1;
+    int index = size;
+    arr[index] = val;
+
+    while (index > 1) {
+      int parent = index / 2;
+
+      if (arr[parent] < arr[index]) {
+        swap(arr[parent], arr[index]);
+        index = parent;
+      } else {
+        return;
+      }
+    }
+  }
+  void print() {
+    for (int i = 1; i <= size; i++) {
+      cout << arr[i] << " ";
+    }
+  }
+};
+int main() {
+  Heap h;
+  h.insert(50);
+  h.insert(55);
+  h.insert(53);
+  h.insert(52);
+  h.print();
+  return 0;
 }
