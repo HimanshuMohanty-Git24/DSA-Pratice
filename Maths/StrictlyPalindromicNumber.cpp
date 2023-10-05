a600726e62ee44aa328510bfa32dd9d3c99522bc
HimanshuMohanty-Git24 <codehimanshu24@gmail.com>
2023-10-05 19:12:17 +0530

    2396. Strictly Palindromic Number
    Co-authored-by: Himanshu Mohanty <codehimanshu24@gmail.com>

--- Maths/StrictlyPalindromicNumber.cpp
+++ Maths/StrictlyPalindromicNumber.cpp
@@ -1,10 +1,29 @@
 // 2396. Strictly Palindromic Number
 #include <bits/stdc++.h>
 using namespace std;
+bool isPalindrome(string &s){
+    int n = s.length();
+    for(int i=0;i<n/2;i++){
+        if(s[i]!=s[n-i-1])
+            return false;
+    }
+    return true;
+}
 bool isStrictlyPalindromic(int n)
 {
     int lb = 2;
     int ub = n - 2;
+    for(int i=lb;i<=ub;i++){
+        string s = "";
+        int temp = n;
+        while(temp){
+            s += (temp%i);
+            temp /= i;
+        }
+        if(!isPalindrome(s))
+            return false;
+    }
+    return true;
 }
 int main()
 {
