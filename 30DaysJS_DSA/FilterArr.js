ea686466102da4b5a5312349c17527a4e0fa5330
HimanshuMohanty-Git24 <codehimanshu24@gmail.com>
2023-10-08 20:33:27 +0530

    2634. Filter Elements from Array
    Co-authored-by: Himanshu Mohanty <codehimanshu24@gmail.com>

---
+++ 30DaysJS_DSA/FilterArr.js
@@ -0,0 +1,15 @@
+//2634. Filter Elements from Array
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var filter = function(arr, fn) {
+    let newArr = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (fn(arr[i],i)) {
+            newArr.push(arr[i]);
+        }
+    }
+    return newArr;
+};
