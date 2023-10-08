b3e12a6d44e219956f730be6179880c1e6681231
HimanshuMohanty-Git24 <codehimanshu24@gmail.com>
2023-10-09 01:38:06 +0530

    509. Fibonacci Number
    Co-authored-by: Himanshu Mohanty <codehimanshu24@gmail.com>

---
+++ Leetcode/FibonacciNumber.cpp
@@ -0,0 +1,16 @@
+//509. Fibonacci Number
+#include<bits/stdc++.h>
+using namespace std;
+int fib(int n) {
+    if(n==0 || n==1){
+        return n;
+    }
+    return fib(n-1)+fib(n-2);
+}
+int main()
+{
+    int n;
+    cin>>n;
+    cout<<fib(n)<<endl;
+   return 0;
+}
