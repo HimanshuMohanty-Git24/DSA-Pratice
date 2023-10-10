42efe71e1694c8a5c9fecba6bb5676ee12cccbec
HimanshuMohanty-Git24 <codehimanshu24@gmail.com>
2023-10-10 14:54:49 +0530

    2629. Function Composition

---
+++ 30DaysJS_DSA/FunctionComposition.js
@@ -0,0 +1,21 @@
+//2629. Function Composition
+/**
+ * @param {Function[]} functions
+ * @return {Function}
+ */
+var compose = function(functions) {
+
+	return function(x) {
+        let result = x;
+
+        for (let i = functions.length - 1; i >= 0; i--) {
+            result = functions[i](result);
+        }
+
+        return result;
+    }
+};
+
+
+const fn = compose([x => x + 1, x => 2 * x])
+console.log(fn(4)) // 9
