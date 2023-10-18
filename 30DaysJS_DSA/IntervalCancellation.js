9dc4546b039c6c021a1b8b982eed09d17e9c7140
HimanshuMohanty-Git24 <codehimanshu24@gmail.com>
2023-10-18 10:54:27 +0530

    2725. Interval Cancellation
    Co-authored-by: Himanshu Mohanty <codehimanshu24@gmail.com>

---
+++ 30DaysJS_DSA/IntervalCancellation.js
@@ -0,0 +1,52 @@
+//2725. Interval Cancellation
+/**
+ * @param {Function} fn
+ * @param {Array} args
+ * @param {number} t
+ * @return {Function}
+ */
+var cancellable = function(fn, args, t) {
+    var timerId;
+    var time = 0;
+    var result = [];
+
+    function executeFn() {
+        var returnedValue = fn.apply(null, args);
+        result.push({ time: time, returned: returnedValue });
+        time += t;
+    }
+
+    function cancelFn() {
+        clearInterval(timerId);
+    }
+
+    executeFn();
+    timerId = setInterval(executeFn, t);
+
+    return cancelFn;
+};
+
+
+   const result = []
+
+   const fn = (x) => x * 2
+   const args = [4], t = 35, cancelT = 190
+   const start = performance.now()
+   const log = (...argsArr) => {
+       const diff = Math.floor(performance.now() - start)
+       result.push({"time": diff, "returned": fn(...argsArr)})
+   }
+   const cancel = cancellable(log, args, t);
+   setTimeout(() => {
+      cancel()
+   }, cancelT)
+   setTimeout(() => {
+     console.log(result)  // [
+                          //      {"time":0,"returned":8},
+                          //      {"time":35,"returned":8},
+                          //      {"time":70,"returned":8},
+                          //      {"time":105,"returned":8},
+                          //      {"time":140,"returned":8},
+                          //      {"time":175,"returned":8}
+                          //  ]
+   }, cancelT + t + 15)    
