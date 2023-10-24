6acf9be70ea47b6a0ecaa0d9537232ea4b088744
HimanshuMohanty-Git24 <codehimanshu24@gmail.com>
2023-10-24 09:39:54 +0530

    2727. Is Object Empty
    Co-authored-by: Himanshu Mohanty <codehimanshu24@gmail.com>

---
+++ 30DaysJS_DSA/isObjectEntry.js
@@ -0,0 +1,9 @@
+//2727. Is Object Empty
+/**
+ * @param {Object|Array} obj
+ * @return {boolean}
+ */
+var isEmpty = function(obj) {
+    // your code here
+    return Object.keys(obj).length === 0;
+};
