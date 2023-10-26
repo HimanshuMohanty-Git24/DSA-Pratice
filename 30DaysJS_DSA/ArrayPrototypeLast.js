cd87b24ada091055ab48139affafea88a09617e8
HimanshuMohanty-Git24 <codehimanshu24@gmail.com>
2023-10-26 20:35:12 +0530

    2619. Array Prototype Last
    Co-authored-by: Himanshu Mohanty <codehimanshu24@gmail.com>

---
+++ 30DaysJS_DSA/ArrayPrototypeLast.js
@@ -0,0 +1,15 @@
+//2619. Array Prototype Last
+/**
+ * @return {null|boolean|number|string|Array|Object}
+ */
+Array.prototype.last = function() {
+    if(this.length==0){
+        return -1;
+    }
+    return this[this.length - 1];
+};
+
+/**
+ * const arr = [1, 2, 3];
+ * arr.last(); // 3
+ */
