a121a01ed6c70e3f5ca0731a80d0c65589a813ce
HimanshuMohanty-Git24 <codehimanshu24@gmail.com>
2023-12-10 21:00:06 +0530

    Add CountSubarrayXor.cpp implementation
    Co-authored-by: Himanshu Mohanty <codehimanshu24@gmail.com>

---
+++ Leetcode/CountSubarrayXor.cpp
@@ -0,0 +1,35 @@
+//Number of Subarrays with xor K
+#include<bits/stdc++.h>
+using namespace std;
+//Prefix XOR method:
+int subarraysWithXorK(vector<int> a, int k) {
+    int n = a.size(); //size of the given array.
+    int xr = 0;
+    map<int, int> mpp; //declaring the map.
+    mpp[xr]++; //setting the value of 0.
+    int cnt = 0;
+
+    for (int i = 0; i < n; i++) {
+        // prefix XOR till index i:
+        xr = xr ^ a[i];
+
+        //By formula: x = xr^k:
+        int x = xr ^ k;
+
+        // add the occurrence of xr^k
+        // to the count:
+        cnt += mpp[x];
+
+        // Insert the prefix xor till index i
+        // into the map:
+        mpp[xr]++;
+    }
+    return cnt;
+}
+int main()
+{
+    vector<int> nums={4,2,2,6,4};
+    int k=6;
+    cout<<subarraysWithXorK(nums,k);
+   return 0;
+}
