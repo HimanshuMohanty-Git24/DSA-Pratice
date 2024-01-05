ba7969bf813906263414e6fe0d8badf07471241b
Himanshu Mohanty <codehimanshu24@gmail.com>
2024-01-05 22:56:54 +0530

    Add fundamental datatypes and examples in DataType.py

---
+++ Python/Basics/DataType.py
@@ -0,0 +1,21 @@
+#Fundamental Datatypes in Python
+int #integer
+float #decimal
+bool #boolean
+complex #complex number
+str #string
+list #array (mutable)
+#eg: list = [1,2,3,4,5]
+tuple #list but immutable (can't be changed)
+#eg: tuple = (1,2,3,4,5)
+set #collection of unique values
+#eg: set = {1,2,3,4,5}
+dict #dictionary (key value pairs)
+#eg: dict = {'a':1, 'b':2, 'c':3}
+
+#classes -> custom types
+
+#specialized data types -> modules
+
+#None -> absence of value
+None
