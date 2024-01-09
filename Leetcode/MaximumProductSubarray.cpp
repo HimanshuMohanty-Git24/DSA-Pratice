3744e7d83aa3296e8c66a34668364e6a2461ae11
Himanshu Mohanty <codehimanshu24@gmail.com>
2024-01-09 12:21:25 +0530

    Add logic explanation for finding maximum product subarray
    Co-authored-by: Himanshu Mohanty <codehimanshu24@gmail.com>

--- Leetcode/MaximumProductSubarray.cpp
+++ Leetcode/MaximumProductSubarray.cpp
@@ -14,6 +14,13 @@ int maxProduct(vector<int>& nums) {
     }
     return ans;
 }//Time Complexity: O(n) Space Complexity: O(1)
+/*
+Logic:
+1. If there are even number of negative numbers, then the product of all the numbers is the answer.
+2. If there are odd number of negative numbers, then the answer is either the product of all the numbers except the first negative number or the product of all the numbers except the last negative number.
+3. If there are no negative numbers, then the answer is the product of all the numbers.
+4.if 0 is present in the array, then the answer is the maximum of the above three answers.
+*/
 int main()
 {
     vector<int> nums = {2,3,-2,4};
