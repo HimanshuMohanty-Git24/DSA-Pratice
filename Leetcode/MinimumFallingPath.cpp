14bcb3488577e6db9241d4a95a5ee11055d10481
Himanshu Mohanty <codehimanshu24@gmail.com>
2024-01-19 22:02:16 +0530

    Add MinimumFallingPath.cpp implementation

---
+++ Leetcode/MinimumFallingPath.cpp
@@ -0,0 +1,22 @@
+//931. Minimum Falling Path Sum
+#include<bits/stdc++.h>
+using namespace std;
+int minFallingPathSum(vector<vector<int>>& A) {
+    int n = A.size();
+        vector<int> prev(n);
+        for(int col = 0; col<n; col++)
+            prev[col] = A[0][col];
+        
+        for(int row = 1; row<n; row++) {
+            vector<int> curr(n);
+            for(int col = 0; col<n; col++) {
+                curr[col] = A[row][col] + min({prev[max(0, col-1)],  prev[col],  prev[min(n-1, col+1)]});
+            }
+            prev = curr;
+        }
+        return *min_element(prev.begin(), prev.end());
+}
+int main()
+{
+   return 0;
+}
