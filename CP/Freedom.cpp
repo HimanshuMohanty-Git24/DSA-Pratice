ebe5122496ed5fc77ea7ba537b5b94bc2e7d618f
Himanshu Mohanty <codehimanshu24@gmail.com>
2024-03-20 22:22:48 +0530

    Refactor Freedom.cpp to improve readability and performance

--- CP/Freedom.cpp
+++ CP/Freedom.cpp
@@ -1,42 +1,41 @@
-//Freedom
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <unordered_map>
 using namespace std;
-bool isaArithemeticProgression(int a , int b, int c){
-    if(a-b==c-b){
-        return true;
-    }
-    else{
-        return false;
-    }
+
+bool isaArithmeticProgression(long long a, long long b, long long c) {
+    return (2 * b == a + c);
 }
-void Freedom(){
+
+void Freedom() {
     int n;
-    cin>>n;
-    int arr[n];
-    for(int i=0;i<n;i++){
-        cin>>arr[i];
+    cin >> n;
+    vector<long long> arr(n); // Using vector for dynamic array size
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
     }
-    int ctr=0;
-    for (int i = 0; i < n-1; i++)
-    {
-        for (int j = i; j < n; j++)
-        {
-            int a,b,c;
-            a= arr[i]-arr[j];
-            b= arr[i]+arr[j];
-            c= arr[j]-arr[i];
-            if(isaArithemeticProgression(a,b,c)){
-                ctr++;
+    unordered_map<long long, int> count;
+    long long ctr = 0;
+    for(int i = 0; i < n; i++) {
+        for(int j = i + 1; j < n; j++) {
+            long long a = arr[i] - arr[j];
+            long long b = arr[i] + arr[j];
+            long long c = arr[i] * arr[j];
+            if(isaArithmeticProgression(a, b, c)) {
+                long long hash_val = a * 1000000000LL + b;
+                ctr += count[hash_val];
             }
+            count[b * 1000000000LL + c]++;
         }
     }
-    cout<<ctr<<endl;
+    cout << ctr << endl;
 }
+
 int main() {
-	int T;
-    cin>>T;
-    while(T--){
+    int T;
+    cin >> T;
+    while(T--) {
         Freedom();
     }
-
+    return 0;
 }
