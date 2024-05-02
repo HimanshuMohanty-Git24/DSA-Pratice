80d88989a3c5a126fa282fcde6326156f197885b
Himanshu Mohanty <codehimanshu24@gmail.com>
2024-05-02 13:21:40 +0530

    Remove commented code in Leetcode/tempCodeRunnerFile.cpp

--- Leetcode/tempCodeRunnerFile.cpp
+++
@@ -1,8 +0,0 @@
-<<<<<<< HEAD
-for (int i = 0; i < ans.size(); i++)
-    // {
-    //     cout << ans[i] << " ";
-    // }
-=======
-cout<<majorityElement(nums);
->>>>>>> 050c1ab3ab06147a941498f1159d2d7743e13da1
