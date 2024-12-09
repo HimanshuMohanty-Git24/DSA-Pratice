ad21a2022bc106fa35ced09f8157f74367341aeb
Himanshu Mohanty <codehimanshu24@gmail.com>
2024-12-09 18:30:44 +0530

    Refactor isArraySpecial function to use prefix sums for improved efficiency in checking array specialness

--- Leetcode/isArraySpecial.cpp
+++ Leetcode/isArraySpecial.cpp
@@ -2,19 +2,27 @@
 #include<bits/stdc++.h>
 using namespace std;
 vector<bool> isArraySpecial(vector<int>& nums, vector<vector<int>>& queries) {
-    vector<bool> res;
-    for (auto q : queries) {
-        int l = q[0], r = q[1];
-        bool is_special = true;
-        for (int i = l; i < r; i++) {
-            if ((nums[i] % 2) == (nums[i + 1] % 2)) {
-                is_special = false;
-                break;
+        int n = nums.size();
+        vector<int> prefix(n, 0); // Initialize prefix array with size n
+        
+        // Calculate the prefix array
+        for (int i = 1; i < n; i++) {
+            prefix[i] = prefix[i - 1];
+            if ((nums[i - 1] % 2 == 0 && nums[i] % 2 == 0) || (nums[i - 1] % 2 != 0 && nums[i] % 2 != 0)) {
+                prefix[i]++;
             }
         }
-        res.push_back(is_special);
-    }
-    return res;
+
+        vector<bool> ans; // Result vector
+        
+        // Process the queries
+        for (auto& q : queries) {
+            int left = q[0], right = q[1];
+            int specialCount = prefix[right] - (left > 0 ? prefix[left] : 0);
+            ans.push_back(specialCount == 0);
+        }
+
+        return ans; // Return the result
 }
 int main()
 {
